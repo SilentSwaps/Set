9f155ce843218c73f31e498a6b34e83e80f1dce0
SilentSwaps <leroyveldhoen@gmail.com>
2023-03-03 10:39:40 +0100

    Methods JSdoc

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ declare global {
   }
 }
 
+/** Checks if all given elements are present in the set. */
 Set.prototype.includes = function <T>(o: T[]): boolean {
   for (const item of o) {
     if (!this.has(item)) return false;
@@ -12,6 +13,7 @@ Set.prototype.includes = function <T>(o: T[]): boolean {
   return true;
 };
 
+/** Checks if only some given elements are present in the set. */
 Set.prototype.some = function <T>(o: T[]): boolean {
   for (const item of o) {
     if (this.has(item)) return true;
